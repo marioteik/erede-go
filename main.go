191e193d69b6501aa0913ef98c30f4933522d036
marioteik <mario.teik@gmail.com>
2023-07-05 10:17:56 -0300

    adds authorization

--- main.go
+++ main.go
@@ -1,19 +1,160 @@
 package eredego
 
-import "math/rand"
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+)
 
-const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const sandbox = "https://sandbox-erede.useredecloud.com.br"
+const production = "https://sandbox-erede.useredecloud.com.br"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 // ERedeGo is the main struct of the package
-type ERedeGo struct {}
+type ERedeGo struct{}
 
-// GenerateRandomString generates a random string with the length passed as parameter
-func (e ERedeGo) GenerateRandomString(length int) string {
-	var result string
+// AuthorizationRequest is the struct that contains the request body
+// more documentation at https://developer.userede.com.br/e-rede#endpoint
+type AuthorizationRequest struct {
+	Capture                bool   `json:"capture"`
+	Kind                   string `json:"kind" validate:"oneof=credit debit"`
+	Reference              string `json:"reference" validate:"required,max=16"`
+	Amount                 int64  `json:"amount" validate:"required,max=9999999999,numeric"`
+	Installments           int8   `json:"installments" validate:"min=2,max=12,numeric"`
+	CardholderName         string `json:"kicardholderNamend" validate:"max=30"`
+	CardNumber             string `json:"cardNumber" validate:"required,max=19"`
+	ExpirationMonth        int8   `json:"expirationMonth" validate:"required,max=2,numeric"`
+	ExpirationYear         int16  `json:"expirationYear" validate:"required,max=4,numeric"`
+	SecurityCode           string `json:"securityCode" validate:"max=4"`
+	SoftDescriptor         string `json:"softDescriptor" validate:"max=18"`
+	Subscription           bool   `json:"subscription"`
+	Origin                 int8   `json:"origin" validate:"max=2,numeric"`
+	DistributorAffiliation int32  `json:"distributorAffiliation"  validate:"max=9,numeric"`
+	BrandTid               string `json:"brandTid"  validate:"max=16"`
+	StorageCard            string `json:"storageCard"  validate:"max=2,numeric"`
+	TransactionCredentials TransactionCredentials
+}
 
-	for i := 0; i < length; i++ {
-		result += string(randomStringSource[rand.Intn(len(randomStringSource))])
+// TransactionCredentials is the struct that contains the credentials for the transaction
+type TransactionCredentials struct {
+	CredentialId string `json:"credentialId" validate:"required,max=2"`
+}
+
+// AuthorizationResponse is the struct that contains the response body
+type AuthorizationResponse struct {
+	ReturnCode        string `json:"returnCode"`
+	ReturnMessage     string `json:"returnMessage"`
+	Reference         string `json:"reference"`
+	Tid               string `json:"tid"`
+	Nsu               string `json:"nsu"`
+	AuthorizationCode string `json:"authorizationCode"`
+	DateTime          string `json:"dateTime"`
+	Amount            int64  `json:"amount"`
+	CardBin           string `json:"cardBin"`
+	Last4             string `json:"last4"`
+	Brand             Brand  `json:"brand"`
+}
+
+// Brand is the struct that contains the brand information
+type Brand struct {
+	Name               string `json:"name"`
+	ReturnCode         string `json:"returnCode"`
+	ReturnMessage      string `json:"returnMessage"`
+	MerchantAdviceCode string `json:"merchantAdviceCode"`
+	AuthorizationCode  string `json:"authorizationCode"`
+	BrandTid           string `json:"brandTid"`
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+// GetAuthorization is the function that makes the request to the API to get the authorization for a transaction
+func (e ERedeGo) GetAuthorization(length int) chan string {
+	const endpoint = "/v1/transactions"
+
+	url := sandbox + endpoint
+
+	authorization := AuthorizationRequest{
+		Capture:                true,
+		Kind:                   "credit",
+		Reference:              "1234567890123456",
+		Amount:                 100,
+		CardholderName:         "John Doe",
+		CardNumber:             "5448280000000007",
+		ExpirationMonth:        12,
+		ExpirationYear:         2026,
+		SecurityCode:           "123",
+		SoftDescriptor:         "SoftDescriptor",
+		Subscription:           false,
+		Origin:                 1,
+		DistributorAffiliation: 0,
+		BrandTid:               "1234567890123456",
+		StorageCard:            "0",
+		TransactionCredentials: TransactionCredentials{
+			CredentialId: "01",
+		},
 	}
-	
-	return result
+
+	authorizationJosn, err := json.Marshal(authorization)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// make the post request with Base Auth
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(authorizationJosn))
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Add("Authorization", "Basic "+basicAuth("72548122", "737df67d33374596b7f199e8329d8a39"))
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var result AuthorizationResponse
+	err = json.Unmarshal([]byte(body), &result)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// print the entire response
+	fmt.Println(string(body))
+
+	// ---------------------------------------------
+
+	r := make(chan string)
+
+	go func() {
+		var result string
+
+		for i := 0; i < length; i++ {
+			result += string(randomStringSource[rand.Intn(len(randomStringSource))])
+		}
+
+		r <- result
+	}()
+
+	return r
 }
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestERedeGo_GenerateRandomString(t *testing.T) {
 	var eredego ERedeGo
 
-	result := eredego.GenerateRandomString(16)
+	result := eredego.GetAuthorization(16)
 
 	if len(result) != 16 {
 		t.Errorf("Expected string length of 16, got %d", len(result))
